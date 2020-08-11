dfcc8649eeb1259eae181d4a3749ced9d06a17bd
zhongzhentao <1158414332@qq.com>
2020-08-11 17:37:25 +0800

    perttier测试

--- src/pages/demo/index.jsx
+++ src/pages/demo/index.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+
 export default class index extends Component {
-  changeState = (obj, callback=()=>{}) => {
-    this.setState(obj,callback);
+  changeState = (obj, callback = () => {}) => {
+    this.setState(obj, callback);
   };
+
   render() {
-    const a=1;
     return <div>xxc</div>;
   }
 }
